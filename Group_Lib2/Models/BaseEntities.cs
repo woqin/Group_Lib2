47269dde2517400443e6a2616f9a422cc5cc2263
woqin <redmi1941@outlook.com>
2022-06-13 08:44:38 +0800

    Add project file

---
+++ Group_Lib2/Models/BaseEntities.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace Group_Lib2.Models
+{
+    public class BaseEntities<T> where T : class, new()
+    {
+        private DbContext Db = DbContextFactory.Create();
+        /// <summary>
+        /// 查询过滤
+        /// </summary>
+        /// <param name="whereLambda"></param>
+        /// <returns></returns>
+        public IQueryable<T> LoadEntities(Expression<Func<T, bool>> whereLambda)
+        {
+            return Db.Set<T>().Where<T>(whereLambda);
+        }
+        /// <summary>
+        /// 分页
+        /// </summary>
+        /// <typeparam name="s"></typeparam>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="totalCount"></param>
+        /// <param name="whereLambda"></param>
+        /// <param name="orderbyLambda"></param>
+        /// <param name="isAsc"></param>
+        /// <returns></returns>
+        public IQueryable<T> LoadPageEntities<s>(int pageIndex,int pageSize,out int totalCount,Expression<Func<T,bool>> whereLambda,Expression<Func<T,s>> orderbyLambda,bool isAsc = false)
+        {
+            var temp = Db.Set<T>().Where<T>(whereLambda);
+            totalCount=temp.Count();
+            if (isAsc)
+            {
+                temp = temp.OrderBy<T, s>(orderbyLambda).Skip<T>((pageIndex - 1) * pageSize).Take<T>(pageSize);
+            }
+            else
+            {
+                temp=temp.OrderByDescending<T,s>(orderbyLambda).Skip<T>((pageIndex - 1) * pageSize).Take<T>(pageSize);
+            }
+            return temp;
+        }
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public bool DeleteEntity(T entity)
+        {
+            Db.Entry<T>(entity).State = System.Data.Entity.EntityState.Deleted;
+            return true;
+        }
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public bool EditEntity(T entity)
+        {
+            Db.Entry<T>(entity).State = System.Data.Entity.EntityState.Modified;
+            return true;
+        }
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public T AddEntity(T entity)
+        {
+            Db.Set<T>().Add(entity);
+            return entity;
+        }
+        public bool SaveChanges()
+        {
+            return Db.SaveChanges() > 0 ? true : false;
+        }
+    }
+}
---
+++ Group_Lib2/Models/DbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace Group_Lib2.Models
+{
+    public static class DbContextFactory
+    {
+        public static DbContext Create()
+        {
+            DbContext dbContext = (DbContext)CallContext.GetData("dbContext");
+            if (dbContext == null)
+            {
+                dbContext = new BookManagerSystemEntities();
+                CallContext.SetData("dbContext", dbContext);
+            }
+            return dbContext;
+        }
+    }
+}
